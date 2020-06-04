c29f9a30398361ce8c28a8759090176afa40c449
JACKSON MOURA <31311370+jacksonpm@users.noreply.github.com>
2020-06-04 12:04:15 -0300

    Update views.py

--- djangossl/views.py
+++ djangossl/views.py
@@ -5,9 +5,9 @@ from djangossl.models import SSL
 def ssl_http(request, chave):
     ssl = SSL.objects.filter(chave=chave)
     if ssl:
-        valor = str(ssl[0].valor)
+        valor = str(ssl[0].valor).replace('\r\n', '\n')
     else:
-        valor = ' '
+        valor = ''
 
     filename = chave + '.txt'
     content = valor
