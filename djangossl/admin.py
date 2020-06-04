71bc2f5de18cb939bc032101ffe3bc4a16df20de
JACKSON MOURA <31311370+jacksonpm@users.noreply.github.com>
2020-06-04 11:43:25 -0300

    Update admin.py

--- djangossl/admin.py
+++ djangossl/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
+from django.db.models import CharField
+from django.forms import Textarea
 
 from djangossl.models import SSL
 
-
 @admin.register(SSL)
 class AdminSSL(admin.ModelAdmin):
-
+    formfield_overrides = {
+        CharField: {'widget': Textarea(attrs={'rows': 2, 'cols': 80, 'style': 'font-size: 15px;'})},
+    }
     def has_module_permission(self, request):
         return request.user.is_superuser
