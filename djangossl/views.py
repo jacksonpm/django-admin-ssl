fb9abad577f2302c61b09084f0005189163a755f
JACKSON MOURA <31311370+jacksonpm@users.noreply.github.com>
2020-06-04 11:46:21 -0300

    Update views.py

--- djangossl/views.py
+++ djangossl/views.py
@@ -5,6 +5,7 @@ def ssl_http(request, chave):
     ssl = SSL.objects.filter(chave=chave)
     if ssl:
         valor = str(ssl[0].valor)
+        valor = '\n\r'.join(valor.split())
     else:
         valor = ''
     return HttpResponse(valor)
